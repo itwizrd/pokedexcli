e58cbafcb06256e95d666c523d497b434afb06fd
itwizrd <29903092+itwizrd@users.noreply.github.com>
2026-02-15 11:26:08 -0500

    repl

--- repl.go
+++ repl.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+func repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		command := cleanInput(scanner.Text())
+		fmt.Println("Your command was:",command[0])
+	}
 }
