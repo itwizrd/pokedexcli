2af6a69d8896c8e49ba823772f25085a51793649
itwizrd <29903092+itwizrd@users.noreply.github.com>
2026-02-17 21:23:30 -0500

    'help' and 'exit' commands

--- repl.go
+++ repl.go
@@ -18,7 +18,56 @@ func repl() {
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		command := cleanInput(scanner.Text())
-		fmt.Println("Your command was:",command[0])
+		input := cleanInput(scanner.Text())
+		//fmt.Println("Your input was:",input[0])
+		if len(input) == 0 {
+			continue
+		}
+		command := getCommands()
+		c, exists := command[input[0]]
+		if exists {
+			err := c.callback()
+			if err != nil {
+				fmt.Printf("error calling %s, error: %v", c.name, err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+}
+
+type cliCommand struct {
+	name		string
+	description	string
+	callback	func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	command := getCommands()
+	for _, c := range command {
+		fmt.Printf("%s: %s\n",c.name,c.description)
+	}
+	return nil
 }
