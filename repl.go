a4719cdf17cfbe00e993a6d044ebe69b706ca2f3
itwizrd <29903092+itwizrd@users.noreply.github.com>
2026-02-17 21:51:19 -0500

    split commands to separate file, cleaned up repl for simplicity

--- repl.go
+++ repl.go
@@ -23,51 +23,16 @@ func repl() {
 		if len(input) == 0 {
 			continue
 		}
-		command := getCommands()
-		c, exists := command[input[0]]
+		c, exists := getCommands()[input[0]]
 		if exists {
 			err := c.callback()
 			if err != nil {
-				fmt.Printf("error calling %s, error: %v", c.name, err)
+				fmt.Println(err)
 			}
+			continue
 		} else {
 			fmt.Println("Unknown command")
+			continue
 		}
 	}
-}
-
-type cliCommand struct {
-	name		string
-	description	string
-	callback	func() error
-}
-
-func getCommands() map[string]cliCommand {
-	return map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
-	command := getCommands()
-	for _, c := range command {
-		fmt.Printf("%s: %s\n",c.name,c.description)
-	}
-	return nil
 }
